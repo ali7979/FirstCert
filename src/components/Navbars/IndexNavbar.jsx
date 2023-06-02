87cc55719ebd989c3f8e5e5bf3e76e561ccf0f29
Zoheb Alam <zohebzob@gmail.com>
2023-06-02 21:10:54 +0530

    Some sort of responsiveness added

--- src/components/Navbars/IndexNavbar.jsx
+++ src/components/Navbars/IndexNavbar.jsx
@@ -1,8 +1,7 @@
-
 import React from "react";
 import classnames from "classnames";
 import { useContext } from "react";
-import { useHistory,Link } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import {
   Button,
   Collapse,
@@ -30,6 +29,11 @@ function IndexNavbar() {
     document.documentElement.classList.toggle("nav-open");
   };
 
+  const handleLinkClick = () => {
+    setNavbarCollapse(false);
+    document.documentElement.classList.remove("nav-open");
+  };
+
   React.useEffect(() => {
     const updateNavbarColor = () => {
       if (
@@ -51,15 +55,13 @@ function IndexNavbar() {
       window.removeEventListener("scroll", updateNavbarColor);
     };
   })
+
   const history = useHistory();
 
   const { theme, setTheme } = useContext(ThemeContext);
 
   const handleToggleDarkMode = () => {
-  
     setTheme(!theme);
-    
-    //setDarkMode(!darkMode);
   };
 
 
@@ -67,26 +69,25 @@ function IndexNavbar() {
     <Navbar className={classnames("fixed-top", navbarColor)} expand="lg">
       <Container>
         <div className="navbar-translate">
-        <NavbarBrand
-  data-placement="bottom"
+          <NavbarBrand
+            data-placement="bottom"
+            tag={Link}
+            to="./index"
+            title="First Cert"
+            style={{ display: "flex", alignItems: "center", letterSpacing: "4px" }}
+          >
+            <img src={logo} style={{ height: "100%", width: "5.5em", }} />
+            <div style={{ marginLeft: "1em", fontFamily: "serif", color: theme ? "white" : "black" }}>
+              <span style={{ fontSize: '1.4em' }}>First Cert</span>
+              <h6 style={{ fontFamily: '"Montserrat", "Helvetica", Arial, sans-serif', fontWeight: "700", marginTop: "-0em", letterSpacing: "1px", color: "#51cbce" }}>Accelerating your growth</h6>
+            </div>
+          </NavbarBrand>
 
-  tag={Link}
-  to="./index"
-  title="First Cert"
-  style={{ display: "flex", alignItems: "center",letterSpacing:"4px" }}
->
-  <img src={logo} style={{ height: "100%", width: "5.5em",}} />
-  <div style={{ marginLeft: "1em" ,fontFamily:"serif",color: theme ? "white" : "black"}}>
-   <span style={{fontSize:'1.4em'}}>First Cert</span> 
-    <h6 style={{ fontFamily:'"Montserrat", "Helvetica", Arial, sans-serif',fontWeight:"700",marginTop: "-0em",letterSpacing:"1px",color:"#51cbce"  }}>Accelerating your growth</h6>
-  </div>
-</NavbarBrand>
-
-<button
-  aria-expanded={navbarCollapse}
-  className={`navbar-toggler ${navbarCollapse ? 'toggled' : ''} ${theme ? 'dark' : ''}`}
-  onClick={toggleNavbarCollapse}
->
+          <button
+            aria-expanded={navbarCollapse}
+            className={`navbar-toggler ${navbarCollapse ? 'toggled' : ''} ${theme ? 'dark' : ''}`}
+            onClick={toggleNavbarCollapse}
+          >
             <span className="navbar-toggler-bar bar1" />
             <span className="navbar-toggler-bar bar2" />
             <span className="navbar-toggler-bar bar3" />
@@ -97,201 +98,243 @@ function IndexNavbar() {
           navbar
           isOpen={navbarCollapse}
         >
-          <Nav navbar >
-            <NavItem >
+          <Nav navbar>
+            <NavItem>
               <NavLink
-               style={{ color: theme ? 'white' : 'black' }}
+                style={{ color: theme ? 'white' : 'black' }}
                 data-placement="bottom"
-               
                 title="Home Page"
                 onMouseEnter={(e) => e.target.style.color = '#57C5B6'}
                 onMouseLeave={(e) => e.target.style.color = theme ? 'white' : 'black'}
                 tag={Link}
-            to="./index"
+                to="./index"
+                onClick={handleLinkClick}
               >
                 <i className="fa fa-home" />Home
-                
               </NavLink>
             </NavItem>
-            <UncontrolledDropdown nav inNavbar  >
-              
-                <DropdownToggle
-                  aria-expanded={false}
-                  aria-haspopup={true}
-                  caret
-                  color="default"
-                  data-toggle="dropdown"
-                  href="#pablo"
-                  id="dropdownMenuButton"
-                  nav
-                  onClick={(e) => e.preventDefault()}
-                  role="button"
-                  style={{ color: theme ? 'white' : 'black' }}
-                  onMouseEnter={(e) => e.target.style.color = '#57C5B6'}
-                  onMouseLeave={(e) => e.target.style.color = theme ? 'white' : 'black'}
-              
-                >
-                  <i class="fa fa-code-branch"></i>
-                  Services
-                </DropdownToggle>
-                <DropdownMenu  aria-labelledby="dropdownMenuButton"
-                        className="dropdown-info">
-                  <li>
-                  <a class="dropdown-item" href="#" onClick={e => e.preventDefault()}>
-              Quality &raquo;
-            </a>
-            <ul class="dropdown-menu dropdown-submenu">
-              <li>
-              <Link
-              className="dropdown-item"
-              
-              to="/quality">ISO 9001</Link>
-              </li>
-           
-                  </ul></li>
-                  <li><a class="dropdown-item" href="#" onClick={e => e.preventDefault()}>
-              Environment &raquo;
-            </a>
-            <ul class="dropdown-menu dropdown-submenu">
-              <li>
-              <Link
-              className="dropdown-item"
-              to="/environment">ISO 14001</Link>
-              </li>
-             
-                  </ul></li>
-                  <li>
-                  <a class="dropdown-item" href="#" onClick={e => e.preventDefault()}>
-              Health and Safety &raquo;
-            </a>
-            <ul class="dropdown-menu dropdown-submenu">
-              <li>
-              <Link
-              className="dropdown-item"
-              
-              to="/iso45001">ISO 45001</Link>
-              </li>
-              <li>
-              <Link
-              className="dropdown-item"
-              to="/hipaa">HIPAA</Link>
-              </li>
-                  </ul></li>
-                  <li>
-                  <a class="dropdown-item" href="#" onClick={e => e.preventDefault()}>
-              Food Safety &raquo;
-            </a>
-            <ul class="dropdown-menu dropdown-submenu">
-              <li>
-              <Link
-              className="dropdown-item"
-              to="/halal">HALAL</Link>
-              </li>
-              <li>
-              <Link
-              className="dropdown-item"
-              to="/haccp">HACCP</Link>
-              </li>
-              <li>
-              <Link
-              className="dropdown-item"
-              to="/iso22000">ISO 22000</Link>
-              </li>
-                  </ul></li>
-
-
-
-                  <li>
-                  <a class="dropdown-item" href="#" onClick={e => e.preventDefault()}>
-              Cyber Security &raquo;
-            </a>
-            <ul class="dropdown-menu dropdown-submenu">
-              <li>
-              <Link
-              className="dropdown-item"
-              to="/aboutus">ISO 27001</Link>
-              </li>
-              <li>
-              <Link
-              className="dropdown-item"
-              to="/aboutus">GDPR</Link>              </li>
-              <li>
-              <Link
-              className="dropdown-item"
-              to="/aboutus">PCI</Link>              </li>
-              <li>
-              <Link
-              className="dropdown-item"
-              to="/aboutus">SOC</Link>              </li>
-               <li>
-              <Link
-              className="dropdown-item"
-              to="/aboutus">VAPT</Link>              </li>
-                  </ul></li>
-                 
-                  <li>
-            <a class="dropdown-item" onClick={e => e.preventDefault()} href="#">
-              Product Certification &raquo;
-            </a>
-            <ul class="dropdown-menu dropdown-submenu">
-              <li>
-              <Link
-              class="dropdown-item"
-             
-              to="/aboutus">CE MARKING</Link>
-              </li>
-          
-                  </ul></li>
-
-
-
-
-                </DropdownMenu>
-              </UncontrolledDropdown>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle
+                aria-expanded={false}
+                aria-haspopup={true}
+                caret
+                color="default"
+                data-toggle="dropdown"
+                href="#pablo"
+                id="dropdownMenuButton"
+                nav
+                onClick={(e) => e.preventDefault()}
+                role="button"
+                style={{ color: theme ? 'white' : 'black' }}
+                onMouseEnter={(e) => e.target.style.color = '#57C5B6'}
+                onMouseLeave={(e) => e.target.style.color = theme ? 'white' : 'black'}
+              >
+                <i class="fa fa-code-branch"></i>
+                Services
+              </DropdownToggle>
+              <DropdownMenu aria-labelledby="dropdownMenuButton" className="dropdown-info">
+                <li>
+                  <a  class="dropdown-item drhead" href="#" onClick={e => e.preventDefault()}>
+                    Quality &raquo;
+                  </a>
+                  <ul class="dropdown-menu dropdown-submenu">
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/quality"
+                        onClick={handleLinkClick}
+                      >
+                        ISO 9001
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a class="dropdown-item drhead" href="#" onClick={e => e.preventDefault()}>
+                    Environment &raquo;
+                  </a>
+                  <ul class="dropdown-menu dropdown-submenu">
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/environment"
+                        onClick={handleLinkClick}
+                      >
+                        ISO 14001
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a class="dropdown-item drhead" href="#" onClick={e => e.preventDefault()}>
+                    Health and Safety &raquo;
+                  </a>
+                  <ul class="dropdown-menu dropdown-submenu">
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/iso45001"
+                        onClick={handleLinkClick}
+                      >
+                        ISO 45001
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/hipaa"
+                        onClick={handleLinkClick}
+                      >
+                        HIPAA
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a class="dropdown-item drhead" href="#" onClick={e => e.preventDefault()}>
+                    Food Safety &raquo;
+                  </a>
+                  <ul class="dropdown-menu dropdown-submenu">
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/halal"
+                        onClick={handleLinkClick}
+                      >
+                        HALAL
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/haccp"
+                        onClick={handleLinkClick}
+                      >
+                        HACCP
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/iso22000"
+                        onClick={handleLinkClick}
+                      >
+                        ISO 22000
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a class="dropdown-item drhead" href="#" onClick={e => e.preventDefault()}>
+                    Cyber Security &raquo;
+                  </a>
+                  <ul class="dropdown-menu dropdown-submenu">
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/aboutus"
+                        onClick={handleLinkClick}
+                      >
+                        ISO 27001
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/aboutus"
+                        onClick={handleLinkClick}
+                      >
+                        GDPR
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/aboutus"
+                        onClick={handleLinkClick}
+                      >
+                        PCI
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/aboutus"
+                        onClick={handleLinkClick}
+                      >
+                        SOC
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/aboutus"
+                        onClick={handleLinkClick}
+                      >
+                        VAPT
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a class="dropdown-item drhead" onClick={e => e.preventDefault()} href="#">
+                    Product Certification &raquo;
+                  </a>
+                  <ul class="dropdown-menu dropdown-submenu">
+                    <li>
+                      <Link
+                        class="dropdown-item"
+                        to="/aboutus"
+                        onClick={handleLinkClick}
+                      >
+                        CE MARKING
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+              </DropdownMenu>
+            </UncontrolledDropdown>
             <NavItem>
               <NavLink
                 data-placement="bottom"
                 tag={Link}
-               to="/aboutus"
-               style={{ color: theme ? 'white' : 'black' }}
-               onMouseEnter={(e) => e.target.style.color = '#57C5B6'}
-               onMouseLeave={(e) => e.target.style.color = theme ? 'white' : 'black'}
-           
-                title="About Us"
+                to="/aboutus"
+                style={{ color: theme ? 'white' : 'black' }}
+                onMouseEnter={(e) => e.target.style.color = '#57C5B6'}
+                onMouseLeave={(e) => e.target.style.color = theme ? 'white' : 'black'}
+                onClick={handleLinkClick}
               >
                 <i className="fa fa-users" />About us
               </NavLink>
             </NavItem>
-
-            
-            
-           
-            <NavItem style={{margin:"1em"}}>
-                <CustomInput
-                  type="switch"
-                  
-                  id="exampleCustomSwitch1"
-                  onChange={handleToggleDarkMode}
-                  name="customSwitch1"
-                
-                  label={<i className="fa fa-moon" style={{fontSize:"1.5em",color: theme ? "white" : "black"}}></i>} />               
+            <NavItem style={{ margin: "1em" }}>
+              <CustomInput
+                type="switch"
+                id="exampleCustomSwitch1"
+                onChange={handleToggleDarkMode}
+                onClick={handleLinkClick}
+                name="customSwitch1"
+                label={<i className="fa fa-moon" style={{ fontSize: "1.5em", color: theme ? "white" : "black" }}></i>}
+              />
             </NavItem>
             <NavItem>
               <Button
                 className="btn-round"
                 color="primary"
-                onClick={() => history.push('./contact')}              
+                onClick={() => 
+                  { setNavbarCollapse(false);
+                    document.documentElement.classList.remove("nav-open");
+                    history.push('./contact')
+                  
+                }}
+
               >
                 <i className="nc-icon nc-circle-10"></i> Contact Us
               </Button>
             </NavItem>
-            
-            
           </Nav>
-       
         </Collapse>
       </Container>
-     
     </Navbar>
   );
 }
